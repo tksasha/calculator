870fc5249f8a8c70b77490375b614f1716dbdd04
Oleksandr Tkachuk <tksasha.me@gmail.com>
2023-11-22 10:31:53 +0200

    refactored tests using assets

--- calculate_test.go
+++ calculate_test.go
@@ -2,108 +2,68 @@ package formula
 
 import (
 	"testing"
-)
 
-const M = "\033[31m`%v` was expected, but it is `%v`\033[0m"
+	"gotest.tools/v3/assert"
+)
 
 func TestCalculateFormula(t *testing.T) {
 	t.Run("when it is valid", func(t *testing.T) {
-		exp := 4.0
-
 		res, _ := Calculate("2+2")
 
-		if exp != res {
-			t.Errorf(M, exp, res)
-		}
+		assert.Equal(t, res, 4.0)
 	})
 
 	t.Run("when it contains spaces", func(t *testing.T) {
-		exp := 4.0
-
 		res, _ := Calculate(" 2 + 2 ")
 
-		if exp != res {
-			t.Errorf(M, exp, res)
-		}
+		assert.Equal(t, res, 4.0)
 	})
 
 	t.Run("when it contains letters", func(t *testing.T) {
-		exp := 4.0
-
 		res, _ := Calculate("a2b+c2d")
 
-		if exp != res {
-			t.Errorf(M, exp, res)
-		}
+		assert.Equal(t, res, 4.0)
 	})
 
 	t.Run("when it is an addition", func(t *testing.T) {
-		exp := 7.0
-
 		res, _ := Calculate("3+4")
 
-		if exp != res {
-			t.Errorf(M, exp, res)
-		}
+		assert.Equal(t, res, 7.0)
 	})
 
 	t.Run("when it is a substraction", func(t *testing.T) {
-		exp := 2.0
-
 		res, _ := Calculate("8-6")
 
-		if exp != res {
-			t.Errorf(M, exp, res)
-		}
+		assert.Equal(t, res, 2.0)
 	})
 
 	t.Run("when it is a multiplication", func(t *testing.T) {
-		exp := 20.0
-
 		res, _ := Calculate("4*5")
 
-		if exp != res {
-			t.Errorf(M, exp, res)
-		}
+		assert.Equal(t, res, 20.0)
 	})
 
 	t.Run("when it is a division", func(t *testing.T) {
-		exp := 3.0
-
 		res, _ := Calculate("9/3")
 
-		if exp != res {
-			t.Errorf(M, exp, res)
-		}
+		assert.Equal(t, res, 3.0)
 	})
 
 	t.Run("when it is empty", func(t *testing.T) {
-		exp := 0.0
-
 		res, _ := Calculate("")
 
-		if exp != res {
-			t.Errorf(M, exp, res)
-		}
+		assert.Equal(t, res, 0.0)
 	})
 
 	t.Run("when it contains brackets", func(t *testing.T) {
-		exp := 32.0
-
 		res, _ := Calculate("(5 + 3) * 4")
 
-		if exp != res {
-			t.Errorf(M, exp, res)
-		}
+		assert.Equal(t, res, 32.0)
 	})
 
 	t.Run("when calculated result is float64", func(t *testing.T) {
-		exp := 111.11
-
 		res, _ := Calculate("42.1 + 69.01")
 
-		if exp != res {
-			t.Errorf(M, exp, res)
-		}
+		assert.Equal(t, res, 111.11)
 	})
 }
